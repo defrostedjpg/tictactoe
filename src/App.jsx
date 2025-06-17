cee2db93d463496453b454a25853308c6a0a48db
defrosted <de@courvix.com>
2025-06-17 22:37:35 +0800

    finishing touches

--- src/App.jsx
+++ src/App.jsx
@@ -77,13 +77,21 @@ function Board({ xIsNext, squares, onPlay}) {
 }
 
 function App() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares])
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1)
     setXIsNext(!xIsNext)
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove)
+    setXIsNext(nextMove % 2 === 0)
   }
 
   const moves = history.map((squares, move) => {
